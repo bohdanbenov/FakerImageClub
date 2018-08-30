deef6790b7d54c19788cd5a62f974085809d7935
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-08-31 01:09:59 +0300

    Edit routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :favourites, only: [:index, :show, :new]
+  root("favourites#index")
+  resources :favourites, only: [:index, :new, :create]
+  resources :allfavourites, only: :index
 end
